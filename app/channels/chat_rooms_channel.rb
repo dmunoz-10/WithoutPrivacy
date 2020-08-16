f58cc96c43244e1499e195d1afa50490780c0c9f
Daniel Muñoz <djmv225@gmail.com>
2020-08-15 21:53:31 -0500

    Some bugs fixed 404 and 500 views changed

--- app/channels/chat_rooms_channel.rb
+++ app/channels/chat_rooms_channel.rb
@@ -9,11 +9,12 @@ class ChatRoomsChannel < ApplicationCable::Channel
   def unsubscribed; end
 
   def seen(data)
-    messages = ChatRoom.find(data['chat_room_id'])
-                       .messages
-                       .where.not(user: current_user)
-                       .where(seen_at: nil)
+    chat_room = ChatRoom.find(data['chat_room_id'])
+    messages = chat_room.messages.where.not(user: current_user).where(seen_at: nil)
     messages.map(&:seen!)
+    other_user = chat_room.user1 == current_user ? chat_room.user2 : chat_room.user1
+    notifications = Notification.all.where(recipient: current_user, actor: other_user, seen_at: nil)
+    notifications.map(&:seen!)
     ActionCable.server.broadcast "chat_rooms_channel_#{data['chat_room_id']}",
                                  type: 'seen', messages_id: messages.pluck(:id)
   end
